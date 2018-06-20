2d7f6bd91dc519ad36e176683871426cd981aeb8
guowenbin <guowenbin@ebupt.com>
2018-06-20 19:38:54 +0800

    Signed-off-by: guowenbin <guowenbin@ebupt.com>

--- src/main/java/com/migu/schedule/Schedule.java
+++ src/main/java/com/migu/schedule/Schedule.java
@@ -1,6 +1,7 @@
 package com.migu.schedule;
 
 
+import com.migu.schedule.constants.DataManagement;
 import com.migu.schedule.constants.ReturnCodeKeys;
 import com.migu.schedule.info.TaskInfo;
 
@@ -13,31 +14,72 @@ public class Schedule {
 
 
     public int init() {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+    	DataManagement.NodeMap.clear();
+    	DataManagement.TaskMap.clear();
+    	return ReturnCodeKeys.E001;
     }
 
 
     public int registerNode(int nodeId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+       int code = ReturnCodeKeys.E000;
+       if(nodeId <= 0){
+    	   code = ReturnCodeKeys.E004;
+       }
+       else if (DataManagement.NodeMap.containsKey(nodeId)) {
+    	   code = ReturnCodeKeys.E005;
+	}
+       else {
+    	   DataManagement.NodeMap.put(nodeId, nodeId+"");
+    	   code = ReturnCodeKeys.E003;
+	}
+       return code;
     }
 
     public int unregisterNode(int nodeId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+       int code = ReturnCodeKeys.E000;
+       if(nodeId <= 0){
+    	   code = ReturnCodeKeys.E004;
+       }
+       else if (!DataManagement.NodeMap.containsKey(nodeId)) {
+    	   code = ReturnCodeKeys.E007;
+	}
+       else {
+    	   DataManagement.NodeMap.remove(nodeId);
+    	   code = ReturnCodeKeys.E006;
+	}
+       return code;
     }
 
 
     public int addTask(int taskId, int consumption) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+    	int code = ReturnCodeKeys.E000;
+    	if (taskId <= 0) {
+    		code = ReturnCodeKeys.E009;
+		}
+    	else if (DataManagement.TaskMap.containsKey(taskId)) {
+    		code = ReturnCodeKeys.E010;
+		}
+    	else {
+    		DataManagement.TaskMap.put(taskId, taskId+"");
+    		code = ReturnCodeKeys.E008;
+		}
+        return code;
     }
 
 
     public int deleteTask(int taskId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+    	int code = ReturnCodeKeys.E000;
+    	if (taskId <= 0) {
+    		code = ReturnCodeKeys.E009;
+		}
+    	else if (!DataManagement.TaskMap.containsKey(taskId)) {
+    		code = ReturnCodeKeys.E012;
+		}
+    	else {
+    		DataManagement.TaskMap.remove(taskId);
+    		code = ReturnCodeKeys.E011;
+		}
+        return code;
     }
 
 
@@ -48,8 +90,11 @@ public class Schedule {
 
 
     public int queryTaskStatus(List<TaskInfo> tasks) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+    	int code = ReturnCodeKeys.E000;
+    	if (tasks == null) {
+			code = ReturnCodeKeys.E016;
+		}
+        return code;
     }
 
 }
---
+++ src/main/java/com/migu/schedule/constants/DataManagement.java
@@ -0,0 +1,11 @@
+package com.migu.schedule.constants;
+
+import java.util.Map;
+
+public class DataManagement {
+
+	public static Map<Integer, String> NodeMap;
+	
+	public static Map<Integer, String> TaskMap;
+	
+}
